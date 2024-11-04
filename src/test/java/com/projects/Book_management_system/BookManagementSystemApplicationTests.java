cbd99d71353a51f4069715bc704aae68bad27016
DenzelSE <t.d.selokela@gmail.com>
2024-11-04 21:09:59 +0200

    Spring boot BMS initiated

---
+++ src/test/java/com/projects/Book_management_system/BookManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.Book_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
